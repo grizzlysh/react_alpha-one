821818bc37ee93c8bdbb877c9976dd51e94790c8
grizzlysh <dev.husseinfaisol@gmail.com>
2023-03-14 00:53:06 +0700

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,35 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material'
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider, EmotionCache } from '@emotion/react'
 
-export default function App({ Component, pageProps }: AppProps) {
+import createEmotionCache from '@/utils/createEmotionCache'
+import theme from '@/utils/theme'
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+// export default function App({ Component, pageProps }: AppProps) {
+export default function MyApp(props: MyAppProps) {
+  const { Component, pageProps, emotionCache = clientSideEmotionCache } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+
+    </CacheProvider>
+  )
   return <Component {...pageProps} />
 }
